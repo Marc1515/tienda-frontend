c3fcf3e412a29f45e27a1b203e17a89acef35e98
Marc España Quinquilla <meq.1515@gmail.com>
2023-08-28 13:18:06 +0200

    En el servicio, creo unas variables 'Gemelas' currentUser y authStatus públicas que van a computar todos los cambios que ocurran en las Signals de sus semejantes, con el objetivo de que no se modifiquen las privadas. El propósito del método del login es devolver un post por el cual contiene el tipado de LoginResponse que en el caso que todo salga bien va a establecer el usuario y el estado de autenticado guardando también el token en el localStorage. En el caso de que salga mal con el operador de RxJS throwError devuelvo el mensaje que llega de mi Backend.

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,8 @@
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -11,11 +12,31 @@ export class AuthService {
   private http = inject(HttpClient);
 
   private _currentUser = signal<User | null>(null);
-  private _authtatus = signal<AuthStatus>();
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+  //! Al mundo exterior = fuera de este servicio
+
+  public currentUser = computed(() => this._currentUser());
+  public authStatus = computed(() => this._authStatus());
 
   constructor() {}
 
   login(email: string, password: string): Observable<boolean> {
-    return of(true);
+    const url = `${this.baseUrl}/auth/login`;
+    const body = { email: email, password: password };
+
+    return this.http.post<LoginResponse>(url, body).pipe(
+      tap(({ user, token }) => {
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token', token);
+        console.log({ user, token });
+      }),
+      map(() => true),
+
+      // Todo: errores
+
+      catchError((err) => throwError(() => err.error.message))
+    );
   }
 }
