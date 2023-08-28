57aac8644728bf0116b23f92f40e2ccab6201d20
Marc España Quinquilla <meq.1515@gmail.com>
2023-08-28 16:33:52 +0200

    Ahora, cuando el método login del componente tenga éxito o sea true se enviará al usuario al dashboard.

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
-
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -11,16 +12,17 @@ import Swal from 'sweetalert2';
 export class LoginPageComponent {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
+  private router = inject(Router);
 
   public myForm: FormGroup = this.fb.group({
     email: ['meq.1515@gmail.com', [Validators.required, Validators.email]],
-    password: ['1234567', [Validators.required, Validators.minLength(6)]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
   login() {
     const { email, password } = this.myForm.value;
     this.authService.login(email, password).subscribe({
-      next: () => console.log('Todo Bien!'),
+      next: () => this.router.navigateByUrl('/dashboard'),
       error: (message) => {
         Swal.fire('Error', message, 'error');
       },
