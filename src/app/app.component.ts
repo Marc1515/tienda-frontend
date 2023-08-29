f64472a9a7840718e77d3c17abd91d0ee3491ac5
Marc España Quinquilla <meq.1515@gmail.com>
2023-08-29 18:25:20 +0200

    En el componente principal creo un efecto que se va a disparar cada vez que el estado del usuario cambie, de este modo hay un control de su estado más sencillo de entender.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
+import { AuthStatus } from './auth/interfaces';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tienda-frontend';
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  public finishedAuthCheck = computed<boolean>(() => {
+    if (this.authService.authStatus() === AuthStatus.checking) {
+      console.log(this.authService.authStatus());
+      return false;
+    }
+
+    return true;
+  });
+
+  public authStatusChangedEffect = effect(() => {
+    /* console.log('authStatus:', this.authService.authStatus()); */
+    switch (this.authService.authStatus()) {
+      case AuthStatus.checking:
+        return;
+      case AuthStatus.authenticated:
+        this.router.navigateByUrl('/dashboard');
+        return;
+      case AuthStatus.notAuthenticated:
+        this.router.navigateByUrl('/auth/login');
+        return;
+    }
+  });
 }
